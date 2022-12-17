521159339b2a42d7f055a28f883a8976a973f34e
Diminatorio <104588396+Diminatorio@users.noreply.github.com>
2022-12-17 21:09:27 +0200

    form logic and stylizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import './App.css';
 import ContactList from "./components/ContactList";
+import Form from './components/Form';
 
 class App extends React.Component {
     state = {
         contacts: [{id: 1, firstName: 'Jake', lastName: 'Jackson', phoneNumber: '+380501234567'},
                    {id: 2, firstName: 'Jacob', lastName: 'Menace', phoneNumber: '+380602334567'},
                    {id: 3, firstName: 'Master', lastName: 'Fu', phoneNumber: '+380901234567'},
-                   {id: 4, firstName: 'Po', lastName: 'Panda', phoneNumber: '+380711234567'}]
+                   {id: 4, firstName: 'Po', lastName: 'Panda', phoneNumber: '+380711234567'}],
+        formVisible: false
     }
 
     deleteContact = (id) => {
         this.setState({ contacts: this.state.contacts.filter((contact) => contact.id !== id) });
+    }
+
+    addContact = (personData) => {
+        this.setState({
+            contacts: [...this.state.contacts,
+                {
+                    id: Date.now(), ...personData
+                }]
+        })
+    }
+
+    onChangeFormVisible = () => {
+        this.setState({formVisible: !this.state.formVisible})
     }
 
   render(){
@@ -19,6 +34,10 @@ class App extends React.Component {
         <div className="App">
             <h1>Contacts</h1>
             <ContactList contacts={this.state.contacts} onDeleteContact={this.deleteContact}/>
+            {this.state.formVisible ?
+                <Form onAddContact={this.addContact} onHideForm={this.onChangeFormVisible}/>:
+                <button onClick={this.onChangeFormVisible} className="show-form">Add contact</button>
+            }
         </div>
     )
   }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import './Form.css'
+
+
+class Form extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+        phoneNumber: ''
+    }
+
+
+    onInputChange = (ev) => {
+        const { value, name } = ev.target;
+        this.setState({
+            ...this.state,
+            [name]: value,
+        });
+    }
+
+    onAddContact = (ev) => {
+        ev.preventDefault();
+        const regex = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?/
+        if (regex.test(this.state.phoneNumber)) {
+            this.props.onAddContact(this.state);
+            this.resetForm();
+            this.props.onHideForm()
+        } else {
+            this.setState({phoneNumber: 'Invalid Number'})
+        }
+
+    }
+
+    resetForm = () => {
+        this.setState({ firstName: '',
+                             lastName: '',
+                             phoneNumber: ''})
+    }
+
+    render(){
+        return (
+            <>
+                <form onSubmit={this.onAddContact} onReset={this.resetForm}>
+                    <div className="text-inputs-container">
+                        <input type="text"
+                               name="firstName"
+                               placeholder="First name"
+                               required
+                               value={this.state.firstName}
+                               onChange={this.onInputChange}/>
+                        <input type="text"
+                               name="lastName"
+                               placeholder="Last name"
+                               required
+                               value={this.state.lastName}
+                               onChange={this.onInputChange}/>
+                        <input type="text"
+                               name="phoneNumber"
+                               placeholder="Phone number"
+                               required
+                               value={this.state.phoneNumber}
+                               onChange={this.onInputChange}/>
+                    </div>
+                    <div className="buttons-container">
+                        <input className="button submit" type="submit" value="Add"
+                               disabled={
+                            !(this.state.firstName !== ''
+                                && this.state.lastName !== ''
+                                && this.state.phoneNumber !=='')? 'true': ''}
+                        />
+                        <input className="button reset" type="reset" value="Clear"/>
+                    </div>
+
+                </form>
+
+            </>
+        )
+    }
+}
+
+export default Form;
